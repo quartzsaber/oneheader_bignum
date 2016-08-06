5d48c86b50bfee46f63a808cfc799a96dc7c7ee8
minary <min050820@gmail.com>
2016-08-06 14:51:02 +0900

    added add()
    Directly written on web interface without any test. Not sure if it works...

--- bignum.h
+++ bignum.h
@@ -6,12 +6,25 @@
 template<int BITS>
 class bignum {
 protected:
-    std::bitset<16> num;
+    std::bitset<BITS> num;
 
 public:
     //bignum(const unsigned intmax_t init);
     //bignum(const bignum& init);
-    bignum& add(const bignum& x);
+    bignum& add(const bignum& x) {
+        //bitwise calculating table.
+        //bits order : carry, a, b
+        //result&1 = x, result&2=carry
+        static const int calctable[7] = {
+            0, 1, 1, 2, 1, 2, 2, 3
+        };
+        bool c=false; //carry
+        for(int i=0; i<BITS; i++) {
+            const bool a=num[i], b=x.num[i];
+            num[i]=calctable[(c<<2)|(b<<1)|(a)]&1;
+            carry=calctable[(c<<2)|(b<<1)|(a)]&2;
+        }
+    }
     bignum& sub(const bignum& x);
     bignum& mul(const bignum& x);
     bignum& div(const bignum& x);
